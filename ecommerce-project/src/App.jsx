161ab2477e3ef8606bb3d85f9baa650509626b16
lemuelmbunwe <lemuelmbunwe@gmail.com>
2026-05-20 00:03:59 +0100

    moved orders and checkout into react. created components and updated to links

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,17 @@
-import { HomePage } from './pages/HomePage';
-import './App.css'
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { HomePage } from "./pages/HomePage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { Route, Routes } from "react-router";
+import "./App.css";
 
 function App() {
- 
-
   return (
-    <HomePage />
-  )
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<OrdersPage />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
