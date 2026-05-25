c2dbd1e1de661528cf7ad7915d6fe1c2f1f738c4
lemuelmbunwe <lemuelmbunwe@gmail.com>
2026-05-25 04:32:06 +0100

    fetching data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ import "./HomePage.css";
 import "./header.css";
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+      return response.json
+    }).then((data)=>{
+      console.log(data);
+    });
   return (
     <>
       <title>Ecommerce Project</title>
