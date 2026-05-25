e8a5f26aa0a89d6032f6947913245f9a91d8c5ae
lemuelmbunwe <lemuelmbunwe@gmail.com>
2026-05-25 04:36:48 +0100

    corrected the code used for fetch

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import "./header.css";
 export function HomePage() {
   fetch('http://localhost:3000/api/products')
     .then((response)=>{
-      return response.json
+      return response.json();
     }).then((data)=>{
       console.log(data);
     });
