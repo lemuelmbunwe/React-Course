73f563763de4902f1cf8ca7a534b515673636d49
lemuelmbunwe <lemuelmbunwe@gmail.com>
2026-05-25 05:17:19 +0100

    get cart from backend and display quantity

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,23 +1,27 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 import "./header.css";
 
 export function HomePage() {
-  const [products , setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-  useEffect(()=>{
-     axios.get('http://localhost:3000/api/products')
-    .then((response)=>{
-      setProducts(response.data)
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
     });
-  })
- 
+
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Ecommerce Project</title>
-      <Header />
+      <Header cart={cart} />
 
       <div className="home-page">
         <div className="products-grid">
@@ -25,10 +29,7 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -40,10 +41,14 @@ export function HomePage() {
                     className="product-rating-stars"
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`}
                   />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents/100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
@@ -73,7 +78,6 @@ export function HomePage() {
               </div>
             );
           })}
-
         </div>
       </div>
     </>
