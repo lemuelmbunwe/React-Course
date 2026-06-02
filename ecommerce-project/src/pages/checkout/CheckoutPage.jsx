9b842679a50f334ba6bfc22962fc4a076fbcaf7a
lemuelmbunwe <lemuelmbunwe@gmail.com>
2026-06-02 17:16:42 +0100

    convert App, homepage and Checkout page from thenables to async await

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -10,14 +10,16 @@ export function CheckoutPage({ cart }) {
   const [paymentSummary, setPaymentSummary] = useState([]);
 
   useEffect(()=>{
-    axios.get('/api/delivery-options?expand=estimatedDeliveryTime').then((response)=>{
+    const fetchCheckoutData = async () => {
+      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
       setDeliveryOptions(response.data);
-    });
 
-    axios.get('/api/payment-summary').then((response)=>{
+      response = await axios.get('/api/payment-summary');
       setPaymentSummary(response.data);
-    });
 
+    }
+    
+    fetchCheckoutData();
   },[])
 
   return (
